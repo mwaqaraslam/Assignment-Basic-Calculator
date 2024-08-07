dfb669cf1671bb6ba7776bad030dd1ceb98b7feb
Muhammad Waqar Aslam - 7433 <waqar.aslam@bankalfalah.com>
2024-08-07 18:22:20 +0500

    first commit

---
+++ var.py
@@ -0,0 +1,19 @@
+message = "This is variable assignment"
+print(message)
+message_1 = "Assign message 1"
+print(message_1)
+message_1 = "second task of point # 2"
+print(message_1)
+name_1 = "Muhammad Waqar Aslam"
+print(name_1)
+name_2 = "Eric"
+print(f"Hello {name_2}, would you like to learn some Python today?")
+poem = """\
+this is Bank, I workin Alfalah
+This is good bank.
+i have 19 years exp."""
+print(poem)
+x= 5
+y =10
+def add(x, y):
+    return x + y
